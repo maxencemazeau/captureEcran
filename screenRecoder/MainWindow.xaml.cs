ba8ed149347c9511b35898a5427fd46c856dce03
Maxence <Maxence@DESKTOP-DD6LG15>
2023-03-23 22:42:49 -0400

    Ajoutez des fichiers projet.

---
+++ screenRecoder/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace screenRecoder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Définit l'événement Click pour le bouton CaptureScreenButton
+            CaptureScreenButton.Click += CaptureScreenButton_Click;
+        }
+
+        private void CaptureScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Prend une capture d'écran de l'ensemble de l'écran
+                var bmpScreenCapture = new Bitmap((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight);
+                var gfxScreenCapture = Graphics.FromImage(bmpScreenCapture);
+                gfxScreenCapture.CopyFromScreen(0, 0, 0, 0, bmpScreenCapture.Size);
+
+                // Affiche la boîte de dialogue pour choisir le nom et l'emplacement du fichier
+                var saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PNG Image (*.png)|*.png";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Enregistre le bitmap dans le fichier choisi par l'utilisateur
+                    bmpScreenCapture.Save(saveFileDialog.FileName, ImageFormat.Png);
+
+                    // Affiche l'image capturée
+                    CapturedImage.Source = BitmapFrame.Create(new MemoryStream(File.ReadAllBytes(saveFileDialog.FileName)));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Une erreur est survenue lors de la capture d'écran : {ex.Message}");
+            }
+        }
+    }
+}
